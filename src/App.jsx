60d718770de62637cfb6ccc731f3f60ac51f5dfb
Renato Lobo <renatolobo_@hotmail.com>
2022-07-10 21:39:11 -0300

    sidebar 1 parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
  import { Header} from './components/Header';
  import {Post} from "./Post"
+
+ import styles from './App.module.css';
+
  import './global.css';
+ import { Sidebar } from './components/Sidebar';
 
  export function App() {
 
   return (
     <>
-    <Header/>
-      <Post 
-        author="Renato" 
-        content="Lôbo"/>       
+      <Header/>
+      <div className={styles.wrapper} >
+        <Sidebar /> 
+        <main>
         <Post 
-        author="Guedes" 
-        content="Alcoforado"/>      
+            author="Renato" 
+            content="Lôbo"/>       
+            <Post 
+            author="Guedes" 
+            content="Alcoforado"/>   
+        </main>
+      </div>   
     </>
    
   )
